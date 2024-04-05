3d742dd8dde72f6768fee2d6fe47bae7eee3f3c1
sw.kim <sw.kim@fusionsoft.co.kr>
2024-04-05 18:01:17 +0900

    오후 6:01 2024-04-05

---
+++ src/main/java/jpabook/springjpashop/Hello.java
@@ -0,0 +1,10 @@
+package jpabook.springjpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Hello {
+    private String data;
+}
---
+++ src/test/java/jpabook/springjpashop/MemberRepositoryTest.java
@@ -0,0 +1,31 @@
+package jpabook.springjpashop;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void testMember() throws Exception {
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        Long savedId = memberRepository.save(member);
+
+        Member findMember = memberRepository.find(savedId);
+
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+    }
+}
