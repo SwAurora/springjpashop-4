a843e19409114e40f37312bc0ec6093da918a125
sw.kim <sw.kim@fusionsoft.co.kr>
2024-04-05 15:46:19 +0900

    오후 3:46 2024-04-05

---
+++ src/main/java/jpabook/springjpashop/SpringjpashopApplication.java
@@ -0,0 +1,13 @@
+package jpabook.springjpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjpashopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringjpashopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jpabook/springjpashop/SpringjpashopApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabook.springjpashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjpashopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
