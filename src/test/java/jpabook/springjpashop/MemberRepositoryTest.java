219a5b6ff033347c183f4d1d55affaa5a6eb70e2
sw.kim <sw.kim@fusionsoft.co.kr>
2024-04-26 10:55:12 +0900

    오전 10:54 2024-04-26

--- src/test/java/jpabook/springjpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/springjpashop/MemberRepositoryTest.java
@@ -1,17 +1,19 @@
 package jpabook.springjpashop;
 
-import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @Transactional
+@Rollback(false)
 class MemberRepositoryTest {
 
     @Autowired
@@ -28,5 +30,6 @@ class MemberRepositoryTest {
 
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
     }
 }
